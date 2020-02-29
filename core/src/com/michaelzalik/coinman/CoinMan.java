0926bc002f917ce49482ddf6dca8470db73a8d9c
mikezalik <mike.zalik@gmail.com>
2020-02-29 08:43:01 -0800

    Adds coin texture and position on screen

--- core/src/com/michaelzalik/coinman/CoinMan.java
+++ core/src/com/michaelzalik/coinman/CoinMan.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CoinMan extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -23,18 +24,34 @@ public class CoinMan extends ApplicationAdapter {
 	ArrayList<Integer> coinX = new ArrayList<Integer>();
 	ArrayList<Integer> coinY = new ArrayList<Integer>();
 	Texture coin;
+	int coinCount;
+
+	Random random;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
+
 		man = new Texture[4];
 		man[0] = new Texture ("frame-1.png");
 		man[1] = new Texture ("frame-2.png");
 		man[2] = new Texture ("frame-3.png");
 		man[3] = new Texture ("frame-4.png");
 
+		coin = new Texture("coin.png");
+
+		random = new Random();
+
 		manY = Gdx.graphics.getHeight() / 2;
+
+
+	}
+
+	public void makeCoin() {
+		float height = random.nextFloat() * Gdx.graphics.getHeight();
+		coinY.add((int)height);
+		coinX.add(Gdx.graphics.getWidth());
 	}
 
 	@Override
