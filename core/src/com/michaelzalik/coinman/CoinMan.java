0ac23a84fd2282b90fd8591d515c005f65188a61
mikezalik <mike.zalik@gmail.com>
2020-02-29 08:09:13 -0800

    Adds integer to track position of sprite in animation

--- core/src/com/michaelzalik/coinman/CoinMan.java
+++ core/src/com/michaelzalik/coinman/CoinMan.java
@@ -10,6 +10,7 @@ public class CoinMan extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
 	Texture[] man;
+	int manState = 0;
 	
 	@Override
 	public void create () {
@@ -26,6 +27,12 @@ public class CoinMan extends ApplicationAdapter {
 	public void render () {
 		batch.begin();
 		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		if (manState < 3) {
+			manState++;
+		} else {
+			manState = 0;
+		}
 
 		batch.draw(man[0], Gdx.graphics.getWidth() / 2 - man[0].getWidth() / 2, Gdx.graphics.getHeight() / 2 - man[0].getHeight() / 2);
 
